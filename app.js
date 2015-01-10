e47c3a9fd4782004b1b69f59396bc0c4aa432ec9
nikolenkoanton92 <nikolenkoanton92@gmail.com>
2015-01-09 16:28:44 -0800

    clean basic firebase chat

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var Firebase = require('firebase');
+var ref = new Firebase('https://yourfireceurl');
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-	res.render('index');
+app.get('/room/:roomId', function(req, res) {
+  var roomId = req.param('roomId');
+  ref.child('room-metadata/').orderByChild('name').equalTo(roomId).on('value', function(snapshot) {
+    if (snapshot.hasChildren()) {
+      snapshot.forEach(function(snap) {
+        var id = snap.child('id').val();
+        res.render('index', {
+          roomId: id
+        });
+
+      });
+    } else {
+      res.render('index', {
+        roomId: ''
+      });
+
+    }
+  });
+
+
 });
 
 app.listen(2000, function() {
-	console.log('listening on *:2000');
+  console.log('listening on *:2000');
 });
